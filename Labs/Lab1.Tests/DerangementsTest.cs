05a5fee669cfdf44ce486fed7e1b38f362c7ed64
zolotyykh <zolotykh.n@knu.ua>
2024-10-08 20:04:59 +0300

    add Tests for Lab1

---
+++ Labs/Lab1.Tests/DerangementsTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab1.Tests;
+
+public class DerangementsTest
+{
+    [Theory]
+    [InlineData(1, 0)]
+    [InlineData(2, 1)]
+    [InlineData(3, 2)]
+    [InlineData(4, 9)]
+    [InlineData(5, 44)]
+    [InlineData(6, 265)]
+    public void CalculateDerangements_ValidInput_ReturnsCorrectResult(int n, long expected)
+    {
+        // Act
+        long result = Derangements.CalculateDerangements(n);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+}
--- Labs/Lab1/Program.cs
+++ Labs/Lab1/Program.cs
@@ -46,21 +46,10 @@ class Program
                 throw new ArgumentOutOfRangeException("Кількість гостей повинна бути в межах від 1 до 100.");
             }
 
-            // Створюємо масив для збереження кількості способів (дерранжментів)
-            long[] dp = new long[n + 1];
-
-            // Базові умови
-            if (n >= 1) dp[1] = 0; // Для одного гостя нема варіантів
-            if (n >= 2) dp[2] = 1; // Для двох гостей є один спосіб
-
-            // Обчислюємо кількість дерранжментів для n гостей
-            for (int i = 3; i <= n; i++)
-            {
-                dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]);
-            }
+            long result = Derangements.CalculateDerangements(n);
 
             // Записуємо результат у файл OUTPUT.TXT
-            File.WriteAllText(outputFilePath, dp[n].ToString());
+            File.WriteAllText(outputFilePath, result.ToString());
 
             Console.WriteLine($"Результат успішно записано у файл {outputFileName}");
         }
